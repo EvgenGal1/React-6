f99d7c060670d10e39c21b16737175b91e0ab30d
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2020-10-24 21:22:59 +0300

    react 1(не раб)

--- 1 - Students/Evgen_Gal/src/index.jsx
+++ 1 - Students/Evgen_Gal/src/index.jsx
@@ -1,57 +1,74 @@
 import React from 'react'
+// импорт класса или объекта
 import ReactDom from 'react-dom'
-
+// отвечает за отображени в DOM
 
 const app = document.querySelector('#app');
 
+// импорт компонента Start.jsx
 import Start from './components/Start/Start.jsx'
 
+// 
 ReactDom.render(
-    <div className="wrapper">
-        <Start name="John" />
-    </div>,
-    app
+	// первое что, второе куда
+	// задавать class в react в jsx то он назыв className 
+	<div className="wrapper">
+		{/* внутри div исползуем компонент Start и пробросываем в нутрь name  */}
+		<Start name="John"/>
+	</div>,
+	// куда
+	app
 )
 
+// // переменная с массивом ссылок
+// let navLinks = ['About', 'Benefits', 'Cotacts']
+// // верстка header по массиву
+// // функция в которую пробросим массив
+// function creareMenuItems(arr){
+// 	// из неё возврашаем массив названий через метод map, чтоб получить title и каждый новый элемент нам будет возвращать элемент версти в <> а в них сам title
+// 	return arr.map((title, i) => <li key = { i }><a>{ title }</a></li>)
+// 	// чтоб небыло ошибок в консол изза раективности reacta, нежно каждому элементу возращаемого методом map, пробросить ему, его же индеск.
+// 	// когда mapим то минимум индек элемента берем вторым параметром в функцию и сообщаем ее дальше в тег в его key (li). (минимум индекс, а так и id - для уникального индетификатора элемента на странице) 
+// 	// return arr.map(title => <li><a>{ title }</a></li>)
+// 	}
 
-// let navLinks = ['About', 'Benefits', 'Contacts'];
-
-// function createMenuItems(arr) {
-//     return arr.map((title, i) => <li key = { i }><a>{ title }</a></li>)
-// }
-
+// // сам header 
+// // внутри тегов находиться функция с проброшеным в неё массивом ссылок.
 // let header = <header>
-//     <nav>
-//         { createMenuItems(navLinks) }
-//     </nav>
+// 	<nav>
+// 		{ creareMenuItems(navLinks) }
+// 	</nav>
 // </header>
+// // на выходе получиться массив элементов (<li><a>{ title }</a></li>), которые будут встроены в div со всем остальным в общий DOM 
 
-// let header = <header>
-//     <nav>
-//         <li><a>1</a></li>
-//         <li><a>2</a></li>
-//         <li><a>3</a></li>
-//     </nav>
-// </header>
+// // старый варик
+// // let header = <header>
+// // 	<nav>
+// // 		<li><a>1</a></li>
+// // 		<li><a>2</a></li>
+// // 		<li><a>3</a></li>
+// // 	</nav>
+// // </header>
 
 // let main = <main>
-//     <section>
-//         <h1>Hello</h1>
-//     </section>
-//     <section>
-//         <h1>React</h1>
-//     </section>
+// 	<section>
+// 		<h1>Hello</h1>
+// 	</section>
+// 	<section>
+// 		<h1>React</h1>
+// 	</section>
 // </main>
 
 // let footer = <footer>
-//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum molestiae neque illum corporis minus repudiandae accusamus natus mollitia. Accusantium adipisci dolor officia quibusdam dicta obcaecati odit a ratione nemo ducimus.</p>
+// <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nostrum maiores itaque, optio facere provident quibusdam earum iste ut error amet, numquam magni, atque quod. Eligendi fuga laudantium porro esse?</p>
 // </footer>
 
+// // метод с 2 параметрами. 1 - что мы хотим отрендарить (можем представлять в различных видах), 2 - где хотим отрендарить
 // ReactDom.render(
-//     <div>
-//         { header }
-//         { main }
-//         { footer }
-//     </div>,
-//     app
+// 	<div>
+// 		{ header }
+// 		{ main }
+// 		{ footer }
+// 	</div>, 
+// 	app
 // )
--- 1 - Students/Evgen_Gal/webpack.config.js
+++ 1 - Students/Evgen_Gal/webpack.config.js
@@ -1,54 +1,79 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// ранее загружаемые плагины. представляют из себя 2 класса.
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// подгружаем их в webpack.config используя функцию require (подключает один модуль к другому). в node.js вместо import используеться require
+// можно использовать copy-webpack-plugin для подгрузки изо локально. документация и инструкции по установке на сайте webpack
 
-const path = require('path');
+const path = require("path");
 
+// эти плагины, из документа (config), экспортируем объект
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, 'src', 'index.jsx')
-        // main: './src/index.jsx'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: '',
-        filename: path.join('js', 'bundle.js')
-    },
-    target: 'web',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.jsx?$/i,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    plugins: [
-                        [
-                            "@babel/plugin-proposal-class-properties",
-                            { "loose": true }
-                        ]
-                    ],
-                    // preset: ["@babel/preset-env", "@babel/preset-react"]
-                }
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: path.join('style', '[name].css'),
-            chunkFilename: '[id].css'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
+  // точка входа.entry point
+  entry: {
+    // main (главный): модуль path (дорожка) resolve (разрешить). path.resolve - сам составляет статический путь из указаных пунктов в скобках. делаеться потому что путь в некоторых системах выглядит по другому. в существуюших папках ищет нужное. указываем специально входной файл .jsx потому что node будет искать только js по умолчанию
+    main: path.resolve(__dirname, "src", "index.jsx"),
+    // можно было указать статический путь =>
+    // main: './src/index.jsx'
+  },
+  // место вывода
+  output: {
+    // поиск, по пути авт создает нужные папки (dist). dirname - общая главная папка
+    path: path.join(__dirname, "dist"),
+    // базавоя ссылка
+    publicPath: "",
+    // (имя файла: путь. присоединиться). если нет, то создаст папку js и файл bundle.js в ней (пакет).
+    filename: path.join("js", "bundle.js"),
+  },
+  // (цель: "сеть"). указывает что мы собираем для web, а не для node или настольных приложений
+  target: "web",
+  // основной компонент webpack.config. указываем правила поведения webpack для опред файлов
+  module: {
+    // правила
+    rules: [
+      {
+        // для css
+        test: /\.css$/i,
+        // плагин MiniCssExtractPlugin.loader (собирает по всему проекту css и создает файлы стилей и прописывает за них пути) и/или css-loader
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        // для js и/или jsx. синтаксис регулярных выражений
+        test: /\.jsx?$/i,
+        // за исключением папки node_modules
+        exclude: /node_modules/,
+        // загрузчик - babel (собирает jsx файлы)
+        loader: "babel-loader",
+        // в опциях
+        options: {
+          // указываем плагины babel. их можно указать в файле .babelrc только в формате json
+          plugins: [
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
+          ],
+          // указано в .babelrc. расписано там же
+          // preset: ["@babel/preset-env", "@babel/preset-react"]
+        },
+      },
     ],
-    devServer: {
-        port: 3300,
-        hot: true,
-        open: false
-    }
-}
+  },
+  plugins: [
+    // настройки css которые передаем по документации webpack
+    new MiniCssExtractPlugin({
+      filename: path.join("style", "[name].css"),
+      chunkFilename: "[id].css",
+    }),
+    // рекомендуемые настройки html которые должны быть. копирует html в которой находиться всё
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      // шаблон: путь. resolve (__ имя каталога, "общедоступный", "index.html"). dirname - общая главная папка. template будет находиться в index.html и он будет использоваться webpack для расположения всего собранного внутрь
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    // главно не порт 300
+    port: 3300,
+    // горячая перезагрузка. по умолчанию true
+    hot: true,
+    // опция dev-servera, открывает вкладку браузера после запуска scripta сборки
+    open: false,
+  },
+};
