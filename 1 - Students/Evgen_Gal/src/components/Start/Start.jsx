a242111c6ad40245374c2b6a5cf02fb355de0b46
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2020-11-13 20:36:42 +0300

    2. 44мин

--- 1 - Students/Evgen_Gal/src/components/Start/Start.jsx
+++ 1 - Students/Evgen_Gal/src/components/Start/Start.jsx
@@ -1,16 +1,39 @@
 import './style.css'
 
 import React from 'react'
+import ReactDom from 'react-dom'
 {/*         <comp nameStaticProp="John Doe" :propDynamic="peremennaya" /> //Vue
             <comp nameStaticProp="John Doe" propDynamic = { peremennaya } /> //React 
 */}
+let arr = [];
 
+function getContent() {
+	arr.push('Some text');
+	ReactDom.render (
+		<Messages messages = { arr }/>,
+		document.querySelector('#test')
+	)
+}
+
+function Messages(props) {
+	let { messages } = props;
+	let msgs = messages.map(text => <p>{ text }</p>);
+
+	return <div>
+					{ messages.length ? msgs : <h1>No messages</h1> }
+			</div>
+}
+
+// функция которую экспортируем, она выдаёт нам вёрстку
 export default props => {
-    let { name } = props;
+	 let { name } = props;
+	 let count = 1;
     return (
-        <div>
-            <h2>From component</h2>
-            <p>Hello, { name }</p>
-        </div>
+		<div>
+			<div id="test">
+				<Messages messages = { arr } />
+			</div>
+			<button onClick={ getContent }>add</button>
+		</div>
     )
 }
